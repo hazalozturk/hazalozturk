803ba2a960f13df4f655fe0947e507cbb81d4531
hazalozturk <hazal.ozturk@troyweb.com>
2020-02-23 12:39:47 -0500

    Add routing  to the project

---
+++ src/components/skillsPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container fluid pt-10>
+        Skills
+    </v-container>
+</template>
+
+<script>
+
+export default {
+  name: "skillsPage",
+  components: {},
+  data: () => ({})
+};
+</script>
