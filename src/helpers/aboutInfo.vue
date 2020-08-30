c8cecb94d14093ce64ce5dd9a70b27b1a4ac3704
hazalozturk <hazal.ozturk@troyweb.com>
2020-08-30 17:41:21 -0400

    Fix typos

--- src/helpers/aboutInfo.vue
+++ src/helpers/aboutInfo.vue
@@ -4,16 +4,23 @@
       <h1>Hi, I’m Hazal.</h1>
       <h2>Developer. Designer.<span class="artist"> Artist.</span></h2>
       <p class="pt-5">
-        I am a front end developer based in Upstate NY who is passionate about
+        I am a front end developer based in Upstate NY, who is passionate about
         re-usable, accessible, and responsive software products. After spending
-        4 years in research, I have retired from Chemsitry with a MS degree and
+        4 years in research, I have retired from Chemistry with a Master's degree and
         I have started learning programming. I have now over two years of
-        development experience in JavaScript, working with incredible people. I
-        am a developer during the day and an abstract Artist after 5pm. I
-        balance my techy personality with my creative perspective. I have worked
-        more than 5 React projects, 2 Laravel projects and 3 Vue projects so
-        far. I have created 10 exhibition flyers and designed many ads.
+        development experience in JavaScript, building responsive web applications
+        as well as mobile applications. Despite my short 2 years journey in programming, 
+        I delivered many software products to a wide range of clientele.
+        I also have created many exhibition flyers, designed many ads and I have been proudly serving
+        as a board member for a local art gallery. I balance my techy personality with my creative 
+        perspective. I am a developer during the day and an abstract artist after 5:00pm.
       </p>
+      <p class="pt-5">
+        Checkout my github and codepen for my development work.
+      </p>
+       <p class="pt-5">
+        Checkout my instagram and gurushots accounts to see my art world.
+      </p>     
     </blockquote>
   </v-flex>
 </template>
--- src/helpers/skillsInfo.vue
+++ src/helpers/skillsInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <v-flex xs12 sm10 md10 pl-15 pr-9 pt-5>
     <blockquote class="blockquote">
-      <h1>Skills that I have:</h1>
+      <h1>My Expertise:</h1>
       <v-row align="center" v-for="(skill, index) in skills" :key="index">
         <v-col md="1" sm="3"
           ><v-progress-circular
@@ -23,12 +23,12 @@ export default {
   data: () => ({
     skills: [
       {
-        title: "CSS, HTML, JS, Bootstrap, JQuery, Responsive Web Design",
+        title: "CSS, HTML, JS, Responsive Web Design, Mobile App Development",
         color: "#2e8540",
         percent: 95,
       },
       {
-        title: "Git, NodeJS, React, webpack, wordpress, Vue, Figma",
+        title: "Git, NodeJS, Webpack, React, React Native, Vue, Wordpress, Figma",
         color: "#004bad",
         percent: 75,
       },
