bac81f073596ec2586cafd2cb367ff89528d8dfc
hazalozturk <hazal.ozturk@troyweb.com>
2020-08-30 16:18:01 -0400

    Separate about page's info section

---
+++ src/helpers/aboutInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-flex xs12 sm10 md10 pl-12 pr-9 pt-5>
+    <blockquote class="blockquote">
+      <h1>Hi, I’m Hazal.</h1>
+      <h2>Developer. Designer.<span class="artist"> Artist.</span></h2>
+      <p class="pt-5">
+        I am a front end developer based in Upstate NY who is passionate about
+        re-usable, accessible, and responsive software products. After spending
+        4 years in research, I have retired from Chemsitry with a MS degree and
+        I have started learning programming. I have now over two years of
+        development experience in JavaScript, working with incredible people. I
+        am a developer during the day and an abstract Artist after 5pm. I
+        balance my techy personality with my creative perspective. I have worked
+        more than 5 React projects, 2 Laravel projects and 3 Vue projects so
+        far. I have created 10 exhibition flyers and designed many ads.
+      </p>
+    </blockquote>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: "AboutInfo",
+  data: () => ({}),
+};
+</script>
+
+<style></style>
