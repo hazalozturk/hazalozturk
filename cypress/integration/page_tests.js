014b9a714d4a1dcf78901090505b42d8d8417f26
hazalozturk <hazal.ozturk@troyweb.com>
2021-08-27 10:05:33 -0400

    Update end to end tests

--- cypress/integration/page_tests.js
+++ cypress/integration/page_tests.js
@@ -8,38 +8,3 @@ describe('Home Page Tests', function() {
       cy.visit('/');
     })
 });
-
-describe('Home Page Tests', function () {
-    it('Home Page has all the pages', function() {
-        cy.visit('/');
-        pages.forEach(function(page) {
-            cy.contains(page);
-        })
-    })
-});
-
-describe('Home Page Tests', function () {
-    it('Home Page has all the links', function() {
-        cy.visit('/');
-        links.forEach(function(link, index) {
-            cy.get(`:nth-child(${index+1}) > .v-list-item__icon > .subheading > .v-btn__content > .v-icon`)
-            .should('have.class',  link)
-        })
-    })
-});
-
-describe('Home Page Tests', function () {
-    it('Visits About Page', function() {
-        cy.visit('/');
-        cy.contains('About').click();
-        cy.url().should('include', '/about')
-    })
-})
-
-describe('Home Page Tests', function () {
-    it('Visits Skills Page', function() {
-        cy.visit('/');
-        cy.contains('Skills').click();
-        cy.url().should('include', '/skills')
-    })
-})
