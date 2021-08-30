6e4c9a9894f3c120c4850020856429cea8abe9ee
hazalozturk <hazal.ozturk@troyweb.com>
2021-08-30 13:00:11 -0400

    Update about info

--- src/helpers/aboutInfo.vue
+++ src/helpers/aboutInfo.vue
@@ -4,17 +4,17 @@
       <h2>About Me</h2>
       <p class="pt-5">
         I am a creative developer based in NYC, who is passionate about
-        re-usable, accessible, and responsive software products. After spending
-        4 years in research, I have retired from Chemistry with a Master's
-        degree and I have started learning programming. I have now over three
-        years of development experience in JavaScript, building responsive web
-        applications as well as mobile applications. Over the last couple years,
-        I delivered many software products to a wide range of clientele.
+        re-usable, accessible, elegant and responsive digital experiences. After
+        spending 4 years in research, I have retired from Chemistry with a
+        Master's degree and I have started learning programming. I have now over
+        three years of development experience in JavaScript, building responsive
+        web applications as well as mobile applications. Over the last couple
+        years, I delivered many software products to a wide range of clientele.
       </p>
       <p>
-        I also have created many exhibition flyers, designed many ads,
-        maintained couple social media campaigns and I have been proudly
-        serving as a board member for Albany Center Gallery.
+        Besides building software products, I also have created many exhibition
+        flyers, designed many ads, maintained couple social media campaigns and
+        I have been proudly serving as a board member for Albany Center Gallery.
       </p>
       <p>
         I balance my techy personality with my creative perspective. I am a
@@ -23,10 +23,21 @@
         5:00pm.
       </p>
       <p class="pt-5">
-        Checkout my github and codepen for my development work.
+        Checkout my
+        <a href="https://github.com/hazalozturk" target="_blank">github</a> and
+        <a href="https://codepen.io/hozturk" target="_blank">codepen</a> for my
+        development work.
       </p>
       <p class="pt-5">
-        Checkout my instagram and gurushots accounts to see my art world.
+        Would you want to see my art world too? Checkout my
+        <a href="https://www.instagram.com/artbyhazal/?hl=en" target="_blank"
+          >instagram</a
+        >
+        and
+        <a href="https://www.gurushots.com/hcanisag/photos" target="_blank"
+          >gurushots</a
+        >
+        accounts to see my art world.
       </p>
     </blockquote>
   </v-row>
